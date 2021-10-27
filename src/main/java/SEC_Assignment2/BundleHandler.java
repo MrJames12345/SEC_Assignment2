b6e0a837b31e6495324be8cce629dba5ce7ef17f
JamesCasella <james.casella@student.curtin.edu.au>
2021-10-27 21:31:49 +0800

    Separated into MainApp and MainGUI. Added BundleHandler and a couple en_AU properties.

---
+++ src/main/java/SEC_Assignment2/BundleHandler.java
@@ -0,0 +1,29 @@
+package SEC_Assignment2;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class BundleHandler {
+    
+    // Variables
+    private static String defaultLocale = "en-AU";
+    private static ResourceBundle staticBundle;
+
+    // Set bundle
+    public static void setBundle(String inLocale)
+    {
+        staticBundle = ResourceBundle.getBundle("bundle", Locale.forLanguageTag(inLocale));
+    };
+
+    // Get string using given label
+    public static String getString(String inLabel)
+    {
+        if (staticBundle == null)
+        {
+            staticBundle = ResourceBundle.getBundle("bundle", Locale.forLanguageTag(defaultLocale));
+        }
+        System.out.println(staticBundle);
+        return staticBundle.getString(inLabel);
+    }
+
+}
